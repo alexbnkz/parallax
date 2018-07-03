1743fb00e1f4ba7370f15ca32541a2d078fc6ce7
Alex B. Santos <alexbenincasa@ymail.com>
2018-07-03 16:30:19 -0300

    versão 3 da pagina metodoplan

--- js/init.js
+++ js/init.js
@@ -6,3 +6,15 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.materialboxed');
+	var instances = M.Materialbox.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+	$('.materialboxed').materialbox();
+});
