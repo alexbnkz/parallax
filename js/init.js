c9d67ccd8a1276c76bf260e8b0fe38c8f3441151
Alex B. Santos <alexbenincasa@ymail.com>
2018-07-03 19:28:05 -0300

    foto azul e texto do quitoplan

--- js/init.js
+++ js/init.js
@@ -3,18 +3,7 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+	$('.materialboxed').materialbox();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-
-document.addEventListener('DOMContentLoaded', function() {
-	var elems = document.querySelectorAll('.materialboxed');
-	var instances = M.Materialbox.init(elems, options);
-});
-
-// Or with jQuery
-
-$(document).ready(function(){
-	$('.materialboxed').materialbox();
-});
